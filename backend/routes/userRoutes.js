4cc928b1abf109028e76e2ef60e7bae624e8cd08
jairo <jairo_cal@yahoo.com>
2021-07-31 19:18:34 -0700

    added loged out api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,14 +10,20 @@ userRouter.post("/", addNewUser);
 // POST request for /api/users/login
 userRouter.post("/login", authUser);
 
+// Restore user Session
 userRouter.get("/restore", restoreUser, (req, res) => {
   const { user } = req;
-//   console.log(user,)
   if (user) {
     return res.json({
       user: user
     });
   } else return res.json({});
 });
+
+// Logout Out User (logged out by clearing cookie)
+userRouter.delete("/logout" , (req, res) => {
+  res.clearCookie("token")
+  return res.json({message: "Logged Out Successfully"})
+})
 
 module.exports = userRouter;
