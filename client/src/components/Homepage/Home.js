12248e2c4cd41b2e88b19060f92a5c20e294580a
jairo <jairo_cal@yahoo.com>
2021-07-31 19:08:46 -0700

    updated splash page

--- client/src/components/Homepage/Home.js
+++ client/src/components/Homepage/Home.js
@@ -1,48 +1,13 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-
-import Login from "../auth/Login";
-import SignUp from "../auth/Signup";
-import { showModal2, setCurrentModal2 } from "../../store/modal2";
-
-import { Button } from "@chakra-ui/react";
-import "./Home.css"
-
+import "./Home.css";
+import Navbar from "../Navbar/Navbar";
 
 function Home() {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user)
-  const showLogin = () => {
-    dispatch(setCurrentModal2(Login));
-    dispatch(showModal2());
-  };
 
-  const showSignup = () => {
-    dispatch(setCurrentModal2(SignUp));
-    dispatch(showModal2());
-  };
 
   return (
     <div className="main">
-    <h1>Welcome {user}!</h1>
-    <a href="/gameboard">GameBoard</a>
-      <div>
-        {!user && (
-          <div>
-            <Button colorScheme="teal" onClick={showLogin}>
-              Login
-            </Button>
-            <Button colorScheme="teal" variant="outline" onClick={showSignup}>
-              Signup
-            </Button>
-          </div>
-        )}
-        {user && (
-          <Button colorScheme="teal" variant="outline">
-            Logout
-          </Button>
-        )}
-      </div>
+      <Navbar />
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Login from "../auth/Login";
+import SignUp from "../auth/Signup";
+import { showModal2, setCurrentModal2 } from "../../store/modal2";
+import { Button } from "@chakra-ui/react";
+
+import "./Navbar.css";
+
+function Navbar() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const showLogin = () => {
+    dispatch(setCurrentModal2(Login));
+    dispatch(showModal2());
+  };
+
+  const showSignup = () => {
+    dispatch(setCurrentModal2(SignUp));
+    dispatch(showModal2());
+  };
+
+  return (
+    <div className="Navbar">
+      <div>
+        <h1>Welcome, {user}!</h1>
+      </div>
+      <Link to="gameboard">
+        <Button colorScheme="teal" variant="outline">
+          GameBoard
+        </Button>
+      </Link>
+      <div>
+        {!user && (
+          <div>
+            <Button colorScheme="teal" onClick={showLogin}>
+              Login
+            </Button>
+            <Button colorScheme="teal" variant="outline" onClick={showSignup}>
+              Signup
+            </Button>
+          </div>
+        )}
+        {user && (
+          <Button colorScheme="teal" variant="outline">
+            Logout
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
