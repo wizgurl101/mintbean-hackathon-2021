333cfc32ab197817a40e6f02de81db09e21e3edd
jairo <jairo_cal@yahoo.com>
2021-08-02 13:59:04 -0700

    initial commit

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Grid, GridItem, Button, HStack, Container, Text } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Button,
+  HStack,
+  Container,
+  Text,
+} from "@chakra-ui/react";
+import "./Card.css";
 
 function Card() {
   const [deck, setDeck] = useState([]);
@@ -9,7 +17,12 @@ function Card() {
   const [playerValue, setPlayerValue] = useState(0);
   const [disable, setDisable] = useState(false);
   const [hide, setHide] = useState(false);
-  const [winScore, setWinScore] = useState(21);
+  const [playingGame, setPlayingGame] = useState(false);
+  const [hideButton, setHideButton] = useState(true);
+  const [youWin, setYouWin] = useState(false);
+  const [youLose, setYouLose] = useState(false);
+  const [Tie, setTie] = useState(false);
+  // const [winScore, setWinScore] = useState(21);
 
   // On start of a solo game
   const startSolo = () => {
@@ -46,12 +59,17 @@ function Card() {
   // Will trigger deck to be made upon loading into the page
   useEffect(() => {
     startSolo();
-  }, []);
+  }, [playingGame]);
 
   const dealCards = () => {
+    setYouWin(false);
+    setYouLose(false);
+    setTie(false);
+    setPlayingGame(true);
+    setHideButton(false);
     setHide(true);
     setDisable(true);
-    console.log(deck);
+    // console.log(deck);
     setDealerHand([]);
     setPlayerHand([]);
     // Assign the cards that will be handed out to the player and dealers for game to start
@@ -84,7 +102,10 @@ function Card() {
   const gameOver = () => {
     setPlayerValue(0);
     setDealerValue(0);
-    startSolo();
+    // startSolo();
+    setHideButton(true);
+    setDisable(false);
+    setPlayingGame(false);
   };
 
   // removes card from deck and brings it to players card
@@ -101,6 +122,7 @@ function Card() {
   useEffect(() => {
     if (playerValue === 21 && dealerValue === 21) {
       console.log("It's a tie!");
+      setTie(true);
       gameOver();
     }
   }, [playerValue, dealerValue]);
@@ -120,7 +142,7 @@ function Card() {
       }
     }
     setDealerValue(dealerVal);
-  }, [dealerHand.length]);
+  }, [dealerHand.length, playingGame]);
 
   // Will constantly keep track players hand and sum up the value
   useEffect(() => {
@@ -140,14 +162,16 @@ function Card() {
 
     if (playerVal > 21) {
       console.log("You lose!");
+      setYouLose(true);
       gameOver();
     }
 
     if (playerVal === 21) {
       console.log("You win!");
+      setYouWin(true);
       gameOver();
     }
-  }, [playerHand.length]);
+  }, [playerHand.length, playingGame]);
 
   // On a player hold we check conditionals to determine winner
   const hold = () => {
@@ -180,9 +204,11 @@ function Card() {
 
     if (dealerCopy < playerCopy || dealerCopy > 21) {
       console.log("You win!");
+      setYouWin(true)
       gameOver();
     } else {
       console.log("You lose!");
+      setYouLose(true)
       gameOver();
     }
   };
@@ -190,11 +216,17 @@ function Card() {
   return (
     <Container mt={16} centerContent>
       <HStack mb={8}>
-        <Button size="lg" mr={16} disabled={disable} onClick={dealCards}>Start Game</Button>
-        <Button size="md" onClick={hitMe}>Hit!</Button>
-        <Button size="md" ml={16} onClick={hold}>Hold</Button>
+        <Button size="lg" mr={16} disabled={disable} onClick={dealCards}>
+          Start Game
+        </Button>
+        <Button disabled={hideButton} size="md" onClick={hitMe}>
+          Hit!
+        </Button>
+        <Button disabled={hideButton} size="md" ml={16} onClick={hold}>
+          Hold
+        </Button>
       </HStack>
-     
+
       {playerValue > 0 && (
         <Text fontSize="xl" textColor="white">
           Player: {playerValue}, Dealer: {dealerValue}
@@ -211,54 +243,63 @@ function Card() {
         fontSize="2xl"
         templateRows="repeat(3, 1fr)"
       >
-        <GridItem/>
+        <GridItem />
         <GridItem align="center">Deck</GridItem>
-        <GridItem/>
+        <GridItem />
       </Grid>
-      <h1>Dealer Cards</h1>
-      {dealerHand.length > 0 &&
-        dealerHand.map((card) => (
-          <Grid
-            bg="#EDF2F7"
-            color="teal"
-            w="100px"
-            h="150px"
-            border="1px"
-            borderRadius="10"
-            fontSize="2xl"
-            templateRows="repeat(3, 1fr)"
-          >
-            <GridItem align="left" ml={2}>
-              {card.split(".")[0]}
-            </GridItem>
-            <GridItem align="center">{card.split(".")[1]}</GridItem>
-            <GridItem align="left" mr={2} mt={4} transform="rotateY(180deg)">
-              {card.split(".")[0]}
-            </GridItem>
-          </Grid>
-        ))}
-      <h1>Player Hand</h1>
-      {playerHand.length > 0 &&
-        playerHand.map((card) => (
-          <Grid
-            bg="#EDF2F7"
-            color="teal"
-            w="100px"
-            h="150px"
-            border="1px"
-            borderRadius="10"
-            fontSize="2xl"
-            templateRows="repeat(3, 1fr)"
-          >
-            <GridItem align="left" ml={2}>
-              {card.split(".")[0]}
-            </GridItem>
-            <GridItem align="center">{card.split(".")[1]}</GridItem>
-            <GridItem align="right" mr={2} mt={4} transform="rotateX(180deg)">
-              {card.split(".")[0]}
-            </GridItem>
-          </Grid>
-        ))}
+      <h1 className="player_description">Dealer Cards</h1>
+      <div className="card_display">
+        {dealerHand.length > 0 &&
+          dealerHand.map((card) => (
+            <Grid
+              bg="#EDF2F7"
+              color="teal"
+              w="100px"
+              h="150px"
+              border="1px"
+              borderRadius="10"
+              fontSize="2xl"
+              templateRows="repeat(3, 1fr)"
+            >
+              <GridItem align="left" ml={2}>
+                {card.split(".")[0]}
+              </GridItem>
+              <GridItem align="center">{card.split(".")[1]}</GridItem>
+              <GridItem align="left" mr={2} mt={4} transform="rotateY(180deg)">
+                {card.split(".")[0]}
+              </GridItem>
+            </Grid>
+          ))}
+      </div>
+      <h1 className="player_description">Player Hand</h1>
+      <div className="card_display">
+        {playerHand.length > 0 &&
+          playerHand.map((card) => (
+            <Grid
+              bg="#EDF2F7"
+              color="teal"
+              w="100px"
+              h="150px"
+              border="1px"
+              borderRadius="10"
+              fontSize="2xl"
+              templateRows="repeat(3, 1fr)"
+            >
+              <GridItem align="left" ml={2}>
+                {card.split(".")[0]}
+              </GridItem>
+              <GridItem align="center">{card.split(".")[1]}</GridItem>
+              <GridItem align="right" mr={2} mt={4} transform="rotateX(180deg)">
+                {card.split(".")[0]}
+              </GridItem>
+            </Grid>
+          ))}
+      </div>
+      <div className="status_text">
+        {youWin && <h1 disabled={youWin}>You Win!</h1>}
+        {youLose && <h1 disabled={youLose}>You Lose!</h1>}
+        {Tie && <h1 disabled={Tie}>Tie!</h1>}
+      </div>
     </Container>
   );
 }
