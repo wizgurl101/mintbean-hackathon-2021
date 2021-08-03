c95164ce1418fb5e01d9038b0b9407e0b8e2fa07
wizgurl101 <nb.phan@hotmail.com>
2021-08-02 20:19:33 -0600

    add footer

--- client/src/components/Homepage/Home.js
+++ client/src/components/Homepage/Home.js
@@ -10,6 +10,23 @@ function Home() {
       <Link to="/about">
         <button className="developers">Development Team</button>
       </Link>
+      <div className="footer">
+        <div>
+          <i class="devicon-react-original-wordmark colored"></i>
+        </div>
+        <div>
+          <i class="devicon-redux-original colored"></i>
+        </div>
+        <div>
+          <i class="devicon-mongodb-plain-wordmark colored"></i>
+        </div>
+        <div>
+          <i class="devicon-express-original-wordmark colored"></i>
+        </div>
+        <div>
+          <i class="devicon-nodejs-plain-wordmark colored"></i>
+        </div>
+      </div>
     </div>
   );
 }
