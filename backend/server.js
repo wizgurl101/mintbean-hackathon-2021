4f2af64194f0294836223674ec19cf66dbefa0ba
jairo <jairo_cal@yahoo.com>
2021-08-05 22:34:38 -0700

    added sockets

--- backend/server.js
+++ backend/server.js
@@ -23,15 +23,86 @@ app.use(cookieParser());
 // accept incoming request object to server as JSON
 app.use(express.json());
 
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true,
-    },
-  })
-);
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+// handle multiplayer
+
+// roomNumber changes to create a new room for the next two players
+let roomNumber = 1;
+
+// function to get current number of clients in a room
+const getNumberOfClients = (roomName) => {
+  return io.sockets.adapter.rooms.get(roomName).size;
+};
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "socketid");
+
+  socket.on("join", (message) => {
+    socket.join("room1");
+    console.log(socket.rooms);
+    // console.log(`${message} has joined`);
+    console.log(socket.id, "socket joined");
+  });
+
+  if (io.sockets.adapter.rooms.get("room1")) {
+    let room = io.sockets.adapter.rooms.get("room1");
+    console.log(room);
+  }
+
+  // socket.on("whatmyid", () => {
+  //   socket.emit('yourid', socket.id)
+  //      io.to("room1").emit("response", "hetttt");
+  // })
+  // socket.emit("yourId", socket.id);
+  // let count = io.of("/").on("room1").sockets.size
+  // console.log(count, "people in room")
+  // socket.emit("peopleinroom")
+  // socket.on("playerValue", (value) => {
+  //   console.log(`From client, player value is ${value}`);
+  // });
+
+  // if (io.sockets.adapter.rooms.get(`room${roomNumber}`)) {
+  //   if (getNumberOfClients(`room${roomNumber}`) < 2) {
+  //     console.log(`room ${roomNumber}  less than 2 if clause`);
+  //     socket.join(`room${roomNumber}`);
+  //     io.to(`room${roomNumber}`).emit(
+  //       "message",
+  //       `new player join room #${roomNumber}`
+  //     );
+  //   } else {
+  //     // create new room
+  //     roomNumber++;
+  //     console.log(`room ${roomNumber}  more than 2 else clause and new room`);
+  //     socket.join(`room${roomNumber}`);
+  //     io.to(`room${roomNumber}`).emit(
+  //       "message",
+  //       `new player join room #${roomNumber}`
+  //     );
+  //   }
+  // } else {
+  //   // create new room
+  //   console.log(`room ${roomNumber}  new room`);
+  //   socket.join(`room${roomNumber}`);
+  //   io.to(`room${roomNumber}`).emit(
+  //     "message",
+  //     `new player join room #${roomNumber}`
+  //   );
+  // }
+
+  // when client disconnects
+  socket.on("disconnect", () => {
+    // send to the player that left
+    socket.emit("message", "Left BlackJack Online Room");
+    // send to the other player
+    socket.broadcast.emit("message", "The other player left the game");
+    socket.removeAllListeners();
+  });
+});
 
 // mount the route(s)
 app.use("/api/users", userRouter);
---
+++ client/src/components/utils/index.js
@@ -0,0 +1,11 @@
+import { io } from "socket.io-client";
+
+export const socket = io("http://localhost:8000");
+export let socketID = "";
+socket.on("connect", () => {
+  socketID = socket.id;
+});
+
+socket.on("response", (message) => {
+  console.log(message);
+});
